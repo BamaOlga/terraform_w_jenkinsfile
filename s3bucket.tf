b6d489b3908d2b34c9c050e971df3db6fbc943b2
BamaOlga <155900279+BamaOlga@users.noreply.github.com>
2024-08-12 19:17:55 +0200

    Update s3bucket.tf

--- s3bucket.tf
+++ s3bucket.tf
@@ -1,6 +1,6 @@
 //create an encrypted s3 bucket to store objects
 resource "aws_s3_bucket" "kina_bucket" {
-  bucket = "kinaterrajens"
+  bucket = "kinaterrajens2024"
 
   tags = {
     Name    = "kinabuck"
